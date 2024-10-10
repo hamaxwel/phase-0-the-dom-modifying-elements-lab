bf4cab0f436d8a273225f94966e0f9157185635e
maxwel kirimi <maxwelkirimii@gmail.com>
2024-10-10 20:10:06 +0300

    document structure

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+
+const main = document.getElementById("main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.textContent = "maxwel is the champion";
+
+
+document.body.appendChild(newHeader);
